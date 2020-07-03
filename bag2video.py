fa8c85abfffe4572c509f486e6688ca500850bdb
yasinalm <yasin_alm_58@hotmail.com>
2020-07-03 19:21:47 +0100

    Upgrade to Python3
    News:
    
    - Also uses 'tqdm' library for the progress bar
    - If 'out_file' is not given, saves to folder where bag file(s) are
    - Removed prints disrupting tqdm

--- bag2video.py
+++ bag2video.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 from __future__ import division
 import rosbag, rospy, numpy as np
 import sys, os, cv2, glob
-from itertools import izip, repeat
+from itertools import  repeat
 import argparse
+
+from tqdm import tqdm
 
 # try to find cv_bridge:
 try:
@@ -16,16 +18,16 @@ except ImportError:
         import roslib; roslib.load_manifest("bag2video")
         from cv_bridge import CvBridge
     except:
-        print "Could not find ROS package: cv_bridge"
-        print "If ROS version is pre-Groovy, try putting this package in ROS_PACKAGE_PATH"
+        print("Could not find ROS package: cv_bridge")
+        print("If ROS version is pre-Groovy, try putting this package in ROS_PACKAGE_PATH")
         sys.exit(1)
 
-def get_info(bag, topic=None, start_time=rospy.Time(0), stop_time=rospy.Time(sys.maxint)):
+def get_info(bag, topic=None, start_time=rospy.Time(0), stop_time=rospy.Time(sys.maxsize)):
     size = (0,0)
     times = []
 
     # read the first message to get the image size
-    msg = bag.read_messages(topics=topic).next()[1]
+    msg = bag.read_messages(topics=topic).__next__()[1]
     size = (msg.width, msg.height)
 
     # now read the rest of the messages for the rates
@@ -42,14 +44,14 @@ def calc_n_frames(times, precision=10):
     intervals = np.diff(times)
     return np.int64(np.round(precision*intervals/min(intervals)))
 
-def write_frames(bag, writer, total, topic=None, nframes=repeat(1), start_time=rospy.Time(0), stop_time=rospy.Time(sys.maxint), viz=False, encoding='bgr8'):
+def write_frames(bag, writer, total, topic=None, nframes=repeat(1), start_time=rospy.Time(0), stop_time=rospy.Time(sys.maxsize), viz=False, encoding='bgr8'):
     bridge = CvBridge()
     if viz:
         cv2.namedWindow('win')
     count = 1
     iterator = bag.read_messages(topics=topic, start_time=start_time, end_time=stop_time)
-    for (topic, msg, time), reps in izip(iterator, nframes):
-        print '\rWriting frame %s of %s at time %s' % (count, total, time),
+    for (topic, msg, time), reps in zip(iterator, nframes):
+        #print('\rWriting frame {} of {} at time {}'.format(count, total, time))
         img = np.asarray(bridge.imgmsg_to_cv2(msg, 'bgr8'))
         for rep in range(reps):
             writer.write(img)
@@ -73,7 +75,7 @@ if __name__ == '__main__':
     parser.add_argument('--viz', '-v', action='store_true', help='Display frames in a GUI window.')
     parser.add_argument('--start', '-s', action='store', default=rospy.Time(0), type=rospy.Time,
                         help='Rostime representing where to start in the bag.')
-    parser.add_argument('--end', '-e', action='store', default=rospy.Time(sys.maxint), type=rospy.Time,
+    parser.add_argument('--end', '-e', action='store', default=rospy.Time(sys.maxsize), type=rospy.Time,
                         help='Rostime representing where to stop in the bag.')
     parser.add_argument('--encoding', choices=('rgb8', 'bgr8', 'mono8'), default='bgr8',
                         help='Encoding of the deserialized image.')
@@ -86,18 +88,18 @@ if __name__ == '__main__':
     if not args.viz:
         imshow = noshow
 
-    for bagfile in glob.glob(args.bagfile):
-        print bagfile
+    for bagfile in tqdm(glob.glob(args.bagfile)):
+        #print(bagfile)
         outfile = args.outfile
         if not outfile:
-            outfile = os.path.join(*os.path.split(bagfile)[-1].split('.')[:-1]) + '.avi'
+            outfile = os.path.join(os.path.split(bagfile)[-2], os.path.split(bagfile)[-1].split('.')[:-1][0]+ '.avi') 
         bag = rosbag.Bag(bagfile, 'r')
-        print 'Calculating video properties'
+        #print('Calculating video properties')
         rate, minrate, maxrate, size, times = get_info(bag, args.topic, start_time=args.start, stop_time=args.end)
         nframes = calc_n_frames(times, args.precision)
         # writer = cv2.VideoWriter(outfile, cv2.cv.CV_FOURCC(*'DIVX'), rate, size)
         writer = cv2.VideoWriter(outfile, cv2.VideoWriter_fourcc(*'DIVX'), np.ceil(maxrate*args.precision), size)
-        print 'Writing video'
+        #print('Writing video')
         write_frames(bag, writer, len(times), topic=args.topic, nframes=nframes, start_time=args.start, stop_time=args.end, encoding=args.encoding)
         writer.release()
-        print '\n'
+        #print('\n')
